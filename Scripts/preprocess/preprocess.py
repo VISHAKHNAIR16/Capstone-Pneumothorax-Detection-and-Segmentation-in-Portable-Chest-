c7d86a7f44570c51b1a09027d64e95f95a0a67ad
VISHAKHNAIR16 <vishakh.22bce7720@vitapstudent.ac.in>
2025-10-22 15:54:28 +0530

    Removed the Data File attached new Dataset and also made scripts folder and structure

---
+++ Scripts/preprocess/preprocess.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import pandas as pd 
+import os 
+import statsmodels.api as sm 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
+
+
+##### Loading Data #####
+
+
+
